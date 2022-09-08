188c72672eb3862d99d3e896f3559c3e76d0a326
Alexey <alexeylosev1501@gmail.com>
2022-09-08 22:48:24 +0500

    created structure AlbumsModel

---
+++ AlbumScreen/App/Model.swift
@@ -0,0 +1,15 @@
+//
+//  Model.swift
+//  AlbumScreen
+//
+//  Created by Алексей Лосев on 08.09.2022.
+//
+
+import Foundation
+
+struct AlbumsModel: Hashable {
+    var albumImage: String
+    var albumName: String?
+    var numberOfPhotosLabel: String?
+}
+
